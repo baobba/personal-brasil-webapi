06a03d1a341a0cb4e2c01578386f6622f451a982
hrangel <kikecomp@gmail.com>
2016-03-03 07:59:49 -0300

    criados sedes para adicionar primeiro admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# ADMINS
+def create_admin_if_new(email, name)
+  admin = Admin.find_by(email: email)
+  if !admin
+    admin = Admin.new(name: name, email: email, password: "12345678", password_confirmation: "12345678")
+    admin.skip_confirmation!
+    admin.save!
+  end
+end
+
+create_admin_if_new("admin@nucleo235.com.br", "Admin Núcleo 235")
